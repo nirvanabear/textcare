b2d0fdbaceb009714052b281c92eaaeeea258a11
nirvanabear <gashwor1@mail.ccsf.edu>
2024-11-30 23:58:49 +0000

    v0.2.0: Implemented capability to send to a user-specified WhatsApp phone number, created new session login page, cleaned up CSS and UI button functions, revamped to hide private data.

--- framework/whatsapp/urls.py
+++ framework/whatsapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('chat/', views.chat, name='chat'),
-    path('send_message/', views.send_message, name='send_message')
+    path('send_message/', views.send_message, name='send_message'),
+    path('chat/open_chat/', views.open_chat, name='open_chat'),
+    path('previous_send_message/', views.previous_send_message, name='previous_send_message'),
+    path('set_chat/', views.set_chat, name='set_chat'),
 ]
